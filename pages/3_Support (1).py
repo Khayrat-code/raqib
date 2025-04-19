dddaefb058e78203fe36b31679179b691b12fea7
Khayrat-code <72605091+Khayrat-code@users.noreply.github.com>
2025-04-19 18:58:46 +0300

    Add files via upload

---
+++ pages/3_Support (1).py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# إعداد الصفحة
+st.set_page_config(page_title="RAQIB Support", layout="centered")
+
+st.title("RAQIB Support | الدعم الفني")
+
+# بيانات التواصل
+st.markdown("### Contact Information | معلومات التواصل")
+st.markdown("**Email:** khayratum@gmail.com")
+st.markdown('**Twitter:** [@Nuclear2024](https://twitter.com/Nuclear2024)')
